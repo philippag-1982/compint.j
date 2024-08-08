fbbf1d7140bb0a84e78975c6a07dbb085ac41a26
Philipp.WindowsDesktop <philippag1982@gmail.com>
2024-08-08 20:21:15 +0200

    Slighly improve `multiplyRussianPeasant`
    by using `addInPlaceAbsGreaterEqual`

--- src/main/java/philippag/lib/common/math/compint/Int9.java
+++ src/main/java/philippag/lib/common/math/compint/Int9.java
@@ -1177,7 +1177,7 @@ public final class Int9 implements Comparable<Int9>, AsciiDigitStreamable, CharS
      */
     private static Int9 multiplyRussianPeasantImpl(Int9 lhs, Int9 rhs) {
         int length = lhs.length + rhs.length;
-        var sum = new Int9(new int[length], length - 1, 1);
+        var sum = new Int9(new int[length]);
 
         // we must copy b/c adding and halving is destructive.
         // we preallocate the rhs with double it's size
@@ -1189,7 +1189,7 @@ public final class Int9 implements Comparable<Int9>, AsciiDigitStreamable, CharS
         do {
             proceed = lhs.compareToAbs(ONE) > 0;
             if (!lhs.isEven()) {
-                sum.addInPlaceAbs(rhs);
+                sum.addInPlaceAbsGreaterEqual(rhs);
             }
             lhs.halfInPlaceImpl();
             rhs.doubleInPlaceImpl();
