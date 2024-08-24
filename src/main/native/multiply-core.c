3c500368044c55b52a9adab64963c4f75bf19f70
Philipp.ThinkPad <philippag1982@gmail.com>
2024-08-24 13:20:32 +0200

    cleanup multiply-core.c

--- src/main/native/multiply-core.c
+++ src/main/native/multiply-core.c
@@ -1,24 +1,7 @@
 #include <jni.h>
 //#include <assert.h>
 
-#ifdef _WIN32
-	#define INT128  __int128_t
-	#define INT64   __int64
-	#define INT32   __int32
-//	#define INT64   long long
-//	#define INT32   long
-#else
-	#define INT128  __int128_t
-	#define INT64   __int64_t
-	#define INT32   __int32_t
-//	#define INT64   jlong
-//	#define INT32   jint
-#endif
-
-#define WIDTH 40
-
 #define BASE32 1000000000
-//#define BASE64 1000000000000000000
 
 JNIEXPORT void JNICALL Java_philippag_lib_common_math_compint_Int9N_multiplyCore(
 		JNIEnv * env,
@@ -31,7 +14,7 @@ JNIEXPORT void JNICALL Java_philippag_lib_common_math_compint_Int9N_multiplyCore
 	int lhsMax = lhsOffset + lhsLength - 1;
 	int rhsMax = rhsOffset + rhsLength - 1;
 	int shift = 1;
-	INT32 carry = 0;
+	jint carry = 0;
 	jboolean isCopy;
 
 	jint * lhs = (*env)->GetPrimitiveArrayCritical(env, lhsArray, &isCopy);
@@ -42,15 +25,15 @@ JNIEXPORT void JNICALL Java_philippag_lib_common_math_compint_Int9N_multiplyCore
 //	assert(!isCopy);
 
 	for (int i = rhsMax; i >= rhsOffset; --i, ++shift) {
-		INT32 rhsValue = rhs[i];
-		INT32 k = resultLength - shift;
+		jint rhsValue = rhs[i];
+		jint k = resultLength - shift;
 
 		for (int j = lhsMax; j >= lhsOffset; --j, --k) {
-			INT64 lhsValue = lhs[j]; // force multiplication in INT64
-			INT64 product = carry + lhsValue * rhsValue;
+			jlong lhsValue = lhs[j]; // force multiplication in jlong
+			jlong product = carry + lhsValue * rhsValue;
 
-			carry =                 (INT32) (product / BASE32);
-			INT32 sum = result[k] + (INT32) (product % BASE32);
+			carry =                 (jint) (product / BASE32);
+			jint sum = result[k] + (jint) (product % BASE32);
 			if (sum >= BASE32) {
 				sum -= BASE32;
 //				assert(sum < BASE32);
